ffaa4b0bd8d1c4585a71ed77476f0efcd3e091b5
Mollie Goforth <molliegoforth818@gmail.com>
2024-02-19 20:43:12 -0600

    read,filter,&delete

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,11 +1,16 @@
+import { getBooks, deleteBook } from '../api/bookData';
+import { showBooks } from '../pages/books';
+
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // TODO: CLICK EVENT FOR DELETING A BOOK
     if (e.target.id.includes('delete-book')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE BOOK', e.target.id);
-        console.warn(e.target.id.split('--'));
+        const [, firebaseKey] = e.target.id.split('--');
+        deleteBook(firebaseKey).then(() => {
+          getBooks().then(showBooks);
+        });
       }
     }
 
